6d2dcc3ff6a7c2032437b89da7f5c4c36919dedb
James <crossoveranx@gmail.com>
2020-09-04 16:09:43 +0200

    API for entire workflow and removal of unsigned certs after tx

--- app/cert_issuer_api.py
+++ app/cert_issuer_api.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 import json
 import os
 import ipfshttpclient
+import uuid
 from fastapi.middleware.cors import CORSMiddleware
 
 import cert_issuer.config
@@ -52,52 +53,124 @@ def remove_certificates():
 
 def update_ipfs_link(token_id, token_uri):
     config = get_config()
+    print(config.unsigned_certificates_dir)
+    
+
     certificate_batch_handler, transaction_handler, connector = \
             ethereum_sc.instantiate_blockchain_handlers(config)
     # calling the smart contract to update the token uri for the token id
-    cert_issuer.issue_certificates.update_token_uri(config, token_id, token_uri, transaction_handler)
+    cert_issuer.issue_certificates.update_token_uri(config, certificate_batch_handler, transaction_handler, token_id, token_uri)
     return
 
-def add_file_ipfs(cert_json):
+def add_file_ipfs(cert_path):
     #Important to put name of IPFS container
     client = ipfshttpclient.connect('/dns/ipfs/tcp/5001')
-    hash = client.add_json(cert_json)
+    hash = client.add(cert_path)
     print(hash)
-    return hash
+    print(hash['Hash'])
+    return hash['Hash']
 
-@app.post("/issue")
+
+##Experimental IPNS - IPNS is still in Alpha and doesn't work.
+def add_file_ipns(ipfsHash):
+    client = ipfshttpclient.connect('/dns/ipfs/tcp/5001')
+    #tempAddress = '/ipns/' + ipfsHash
+    tempAddress = '/ipfs/QmNVbGwachkfbnimSc59twHpo9b9bwYzi8QTyMoRNzM9VY'
+    print(tempAddress)
+    ipnshash = client.name.publish(tempAddress, key="self")
+    print(ipnshash)
+    return ipnshash
+
+@app.post("/issueBloxbergCertificate")
 def issue(createToken: createToken, request: Request):
+    print(request)
     config = get_config()
     certificate_batch_handler, transaction_handler, connector = \
             ethereum_sc.instantiate_blockchain_handlers(config)
-            #Removed File mode from ethereum_sc
-            #bitcoin.instantiate_blockchain_handlers(config, False)
+    
     certificate_batch_handler.set_certificates_in_batch(request.json)
     # delegating the issuing of the certificate to the respective transaction handler, that will call "createCertificate" on the smart contract 
     (tx_id, token_id) = cert_issuer.issue_certificates.issue(config, certificate_batch_handler, transaction_handler, createToken.recipientPublickey)
 
     # file that stores the ipfs hashes of the certificates in the batch
-    ipfs_batch_file = config.ipfs_batch_file
+    ipfs_batch_file = "./data/meta_certificates/" + str(uuid.uuid1()) + '.json'
     #Retrieve file path of certified transaction
     blockchain_file_path = config.blockchain_certificates_dir
-    #fileID = certificate_batch_handler.certificates_to_issue.popitem(last=False)[0]
     json_data = []
-    ipfs_list = []
-    for fileID in certificate_batch_handler.certificates_to_issue:
-        full_path_with_file = str(blockchain_file_path + '/' + fileID + '.json')
+
+    ipfs_object =  {"file_certifications": []}
     
+    for fileID in certificate_batch_handler.certificates_to_issue:
+        full_path_with_file = str(blockchain_file_path + '/' + fileID + '.json')         
+        ipfsHash = add_file_ipfs(full_path_with_file)
+        #add_file_ipns(ipfsHash)
+
         with open(full_path_with_file) as f:
             d = json.load(f)
         #Save JSON Certificate to IPFS
-        ipfsHash = add_file_ipfs(d)
-        ipfs_list.append('http://ipfs.io/ipfs/' + ipfsHash)
+        temp = ipfs_object["file_certifications"]
+        y = {"id": fileID, "ipfsHash": 'http://ipfs.io/ipfs/' + ipfsHash, "sha256": d["SHA256Hash"]}
+        temp.append(y)
         json_data.append(d)
 
-    # write ipfs hash list into the ipfs batch file
-    with open(ipfs_batch_file, w) as file:
-        json.dump(ipfs_list, file)
+    # write ipfs object into the ipfs batch file
+    print(ipfs_object)
+    with open(ipfs_batch_file, 'w') as file:
+        json.dump(ipfs_object, file)
 
     ipfs_batch_hash = add_file_ipfs(ipfs_batch_file)
+    
+    update_ipfs_link(token_id, 'http://ipfs.io/ipfs/' + ipfs_batch_hash)
+
+    return json_data
+
+# Test Issuance Endpoint
+
+@app.post("/issueTest")
+def issue(createToken: createToken, request: Request):
+    config = get_config()
+
+
+
+    certificate_batch_handler, transaction_handler, connector = \
+            ethereum_sc.instantiate_blockchain_handlers(config)
+    
+            #Removed File mode from ethereum_sc
+            #bitcoin.instantiate_blockchain_handlers(config, False)
+    certificate_batch_handler.set_certificates_in_batch(request.json)
+
+    # delegating the issuing of the certificate to the respective transaction handler, that will call "createCertificate" on the smart contract 
+    (tx_id, token_id) = cert_issuer.issue_certificates.issue(config, certificate_batch_handler, transaction_handler, createToken.recipientPublickey)
+
+    # file that stores the ipfs hashes of the certificates in the batch
+    ipfs_batch_file = "./data/meta_certificates/" + str(uuid.uuid1()) + '.json'
+    #Retrieve file path of certified transaction
+    blockchain_file_path = config.blockchain_certificates_dir
+    json_data = []
+
+    ipfs_object =  {"file_certifications": []}
+    
+    for fileID in certificate_batch_handler.certificates_to_issue:
+        full_path_with_file = str(blockchain_file_path + '/' + fileID + '.json')         
+        ipfsHash = add_file_ipfs(full_path_with_file)
+        #add_file_ipns(ipfsHash)
+
+        with open(full_path_with_file) as f:
+            d = json.load(f)
+        #Save JSON Certificate to IPFS
+        temp = ipfs_object["file_certifications"]
+        #ipfs_list.append('http://ipfs.io/ipfs/' + ipfsHash)
+        y = {"id": fileID, "ipfsHash": 'http://ipfs.io/ipfs/' + ipfsHash, "sha256": d["SHA256Hash"]}
+        temp.append(y)
+        json_data.append(d)
+
+    # write ipfs object into the ipfs batch file
+    print(ipfs_object)
+    with open(ipfs_batch_file, 'w') as file:
+        json.dump(ipfs_object, file)
+
+    ipfs_batch_hash = add_file_ipfs(ipfs_batch_file)
+    
     update_ipfs_link(token_id, 'http://ipfs.io/ipfs/' + ipfs_batch_hash)
 
     return json_data
--- app/cert_tools_api.py
+++ app/cert_tools_api.py
@@ -5,9 +5,14 @@ from cert_tools import instantiate_v3_alpha_certificate_batch, create_v3_alpha_c
 from pydantic import BaseModel
 import configargparse
 import os
+import httpx
+import json
+import requests
 
 app = FastAPI()
 
+
+##Testing Post Request
 class Batch(BaseModel):
     publicKey: str
     recipient_name: Optional[str]
@@ -24,8 +29,43 @@ class Batch(BaseModel):
             }
         }
 
+##Full Workflow
 
-@app.post("/createUnsignedCertificateBatch")
+@app.post("/createBloxbergCertificate")
+async def createBloxbergCertificate(batch: Batch):
+    #create_v3_alpha_certificate_template(recipient_name, email)
+    conf = create_v3_alpha_certificate_template.get_config()
+    create_v3_alpha_certificate_template.write_certificate_template(conf, batch.recipient_name, batch.email)
+    conf_instantiate = instantiate_v3_alpha_certificate_batch.get_config()
+    uidArray = instantiate_v3_alpha_certificate_batch.instantiate_batch(conf_instantiate, batch.publicKey, batch.recipient_name, batch.email, batch.SHA256)
+
+    url = "http://cert_issuer_api:80/issueBloxbergCertificate"
+    
+    payload = {"recipientPublickey": batch.publicKey, "unSignedCerts": uidArray}
+    headers = {
+  'Content-Type': 'application/json'
+}
+    payload = json.dumps(payload)
+
+    # TODO: Currently a simple post request, but need to research message queues for microservices
+    response = requests.request("POST", url, headers=headers, data = payload)
+    encodedResponse = response.text.encode('utf8')
+    jsonText = json.loads(encodedResponse)
+
+    # TODO: Make requests Async
+    #async with httpx.AsyncClient() as client:
+    #    r = await client.post(url, data = obj)
+    #    print(r)
+    for x in uidArray:
+        full_path_with_file = str(conf.abs_data_dir + '/' + 'unsigned_certificates/' + x + '.json')
+        print(full_path_with_file)
+        os.remove(full_path_with_file)        
+
+    return jsonText
+
+## Test Certificate Generation Endpoint
+
+@app.post("/createUnsignedCertificateBatchTest")
 def createUnsignedCertificate(batch: Batch):
     #create_v3_alpha_certificate_template(recipient_name, email)
     conf = create_v3_alpha_certificate_template.get_config()
