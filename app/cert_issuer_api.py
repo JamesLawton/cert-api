be38bf552f6204303ed2f44732f1d8ba98a3ed77
James <crossoveranx@gmail.com>
2020-10-29 15:44:12 +0100

    separated generatePDF into new route

--- app/cert_issuer_api.py
+++ app/cert_issuer_api.py
@@ -33,18 +33,17 @@ class createToken(BaseModel):
     recipientPublickey: str
     unSignedCerts: List[str]
     enableIPFS: bool
-    enablePDF: bool
 
-    #Used only for Testing API, not for entire workflow.
+    # Used only for Testing API, not for entire workflow.
     class Config:
         schema_extra = {
             "example": {
                 "recipientPublickey": "0x69575606E8b8F0cAaA5A3BD1fc5D032024Bb85AF",
                 "unSignedCerts": ["45c5caba-378b-49eb-bf24-b0056d300f22", "150ed963-dfad-46ee-b242-dfa2eff98671"],
-                "enableIPFS": True,
-                "enablePDF": True
+                "enableIPFS": True
             }
         }
+
 
 def get_config():
     global config
@@ -52,24 +51,28 @@ def get_config():
         config = cert_issuer.config.get_config()
     return config
 
+
 def update_ipfs_link(token_id, token_uri):
     config = get_config()
     print(config.unsigned_certificates_dir)
     certificate_batch_handler, transaction_handler, connector = \
-            ethereum_sc.instantiate_blockchain_handlers(config)
+        ethereum_sc.instantiate_blockchain_handlers(config)
     # calling the smart contract to update the token uri for the token id
-    cert_issuer.issue_certificates.update_token_uri(config, certificate_batch_handler, transaction_handler, token_id, token_uri)
+    cert_issuer.issue_certificates.update_token_uri(config, certificate_batch_handler, transaction_handler, token_id,
+                                                    token_uri)
     return
 
+
 def add_file_ipfs(cert_path):
-    #Important to put name of IPFS container
+    # Important to put name of IPFS container
     client = ipfshttpclient.connect('/dns/ipfs/tcp/5001')
     hash = client.add(cert_path)
     return hash['Hash']
 
+
 ##Experimental IPNS - IPNS is still in Alpha so it is relatively slow. Not recommended for production
 # TODO: Implement key rotation
-def add_file_ipns(ipfsHash, generateKey, newKey = None):
+def add_file_ipns(ipfsHash, generateKey, newKey=None):
     client = ipfshttpclient.connect('/dns/ipfs/tcp/5001')
     if generateKey is True:
         newKey = str(uuid.uuid1())
@@ -78,20 +81,22 @@ def add_file_ipns(ipfsHash, generateKey, newKey = None):
     ipnshash = client.name.publish(tempAddress, key=newKey, timeout=300)
     return ipnshash, newKey
 
-async def issue_batch_to_blockchain(config, certificate_batch_handler, transaction_handler, recipientPublicKey, tokenURI):
+
+async def issue_batch_to_blockchain(config, certificate_batch_handler, transaction_handler, recipientPublicKey,
+                                    tokenURI):
     (tx_id, token_id) = cert_issuer.issue_certificates.issue(config, certificate_batch_handler, transaction_handler,
                                                              recipientPublicKey, tokenURI)
     return tx_id, token_id
 
 
-#Full Workflow - Called from cert_tools_api
+# Full Workflow - Called from cert_tools_api
 @app.post("/issueBloxbergCertificate")
 async def issue(createToken: createToken, request: Request):
     config = get_config()
     certificate_batch_handler, transaction_handler, connector = \
-            ethereum_sc.instantiate_blockchain_handlers(config)
+        ethereum_sc.instantiate_blockchain_handlers(config)
 
-    #Make sure pdf_certificates folder is clean
+    # Make sure pdf_certificates folder is clean
     try:
         full_path_with_file = str('./data/' + 'pdf_certificates/')
         for file_name in os.listdir(full_path_with_file):
@@ -105,7 +110,7 @@ async def issue(createToken: createToken, request: Request):
     if createToken.enableIPFS is True:
         try:
             ipfs_batch_file = "./data/meta_certificates/" + str(uuid.uuid1()) + '.json'
-            ipfs_object =  {"file_certifications": []}
+            ipfs_object = {"file_certifications": []}
             ipfsHash = add_file_ipfs("./data/meta_certificates/.placeholder")
             generateKey = True
             ipnsHash, generatedKey = add_file_ipns(ipfsHash, generateKey)
@@ -118,17 +123,18 @@ async def issue(createToken: createToken, request: Request):
     else:
         tokenURI = 'https://bloxberg.org'
     try:
-        tx_id, token_id = await issue_batch_to_blockchain(config, certificate_batch_handler, transaction_handler, createToken.recipientPublickey, tokenURI)
-        #certificate_batch_handler.set_certificates_in_batch(request.json)
+        tx_id, token_id = await issue_batch_to_blockchain(config, certificate_batch_handler, transaction_handler,
+                                                          createToken.recipientPublickey, tokenURI)
+        # certificate_batch_handler.set_certificates_in_batch(request.json)
         # delegating the issuing of the certificate to the respective transaction handler, that will call "createCertificate" on the smart contract
-        #(tx_id, token_id) = cert_issuer.issue_certificates.issue(config, certificate_batch_handler, transaction_handler, createToken.recipientPublickey, tokenURI)
+        # (tx_id, token_id) = cert_issuer.issue_certificates.issue(config, certificate_batch_handler, transaction_handler, createToken.recipientPublickey, tokenURI)
     except Exception as e:
         print(e)
         return "Issuing unsigned certificate batch to blockchain failed"
-    #Retrieve file path of certified transaction
+    # Retrieve file path of certified transaction
     blockchain_file_path = config.blockchain_certificates_dir
     json_data = []
-    
+
     for fileID in certificate_batch_handler.certificates_to_issue:
         full_path_with_file = str(blockchain_file_path + '/' + fileID + '.json')
         if createToken.enableIPFS is True:
@@ -136,21 +142,13 @@ async def issue(createToken: createToken, request: Request):
 
         with open(full_path_with_file) as f:
             d = json.load(f)
-        #Save JSON Certificate to IPFS
+        # Save JSON Certificate to IPFS
         if createToken.enableIPFS is True:
             temp = ipfs_object["file_certifications"]
             y = {"id": fileID, "ipfsHash": 'http://ipfs.io/ipfs/' + ipfsHash, "crid": d["crid"]}
             temp.append(y)
 
-        if createToken.enablePDF is True:
-            doc = fitz.open('./data/bloxbergDataCertificate.pdf')
-            content = open(full_path_with_file, "rb").read()
-            doc.embeddedFileAdd("bloxbergJSONCertificate", content)
-            doc.save('./data/pdf_certificates/' + fileID + '.pdf', garbage=4, deflate=1)
-
-            # Append to json data struct
-        if createToken.enablePDF is False:
-            json_data.append(d)
+        json_data.append(d)
 
     # write ipfs object into the ipfs batch file
     try:
@@ -162,7 +160,7 @@ async def issue(createToken: createToken, request: Request):
             ipnsHash = add_file_ipns(ipfs_batch_hash, generateKey, newKey=generatedKey)
             print("Updated IPNS Hash")
             print(ipnsHash)
-            #update_ipfs_link(token_id, 'http://ipfs.io/ipfs/' + ipfs_batch_hash)
+            # update_ipfs_link(token_id, 'http://ipfs.io/ipfs/' + ipfs_batch_hash)
     except:
         return "Updating IPNS link failed,"
 
@@ -173,56 +171,5 @@ async def issue(createToken: createToken, request: Request):
             if file_name.endswith('.json'):
                 print(full_path_with_file + file_name)
                 os.remove(full_path_with_file + file_name)
-
-    if createToken.enablePDF is False:
-        return json_data
-    else:
-        return
-
-# Test Issuance Endpoint
-@app.post("/issueTest")
-def issue(createToken: createToken, request: Request):
-    config = get_config()
-
-    certificate_batch_handler, transaction_handler, connector = \
-            ethereum_sc.instantiate_blockchain_handlers(config)
-    
-            #Removed File mode from ethereum_sc
-            #bitcoin.instantiate_blockchain_handlers(config, False)
-    certificate_batch_handler.set_certificates_in_batch(request.json)
-
-    # delegating the issuing of the certificate to the respective transaction handler, that will call "createCertificate" on the smart contract 
-    (tx_id, token_id) = cert_issuer.issue_certificates.issue(config, certificate_batch_handler, transaction_handler, createToken.recipientPublickey)
-
-    # file that stores the ipfs hashes of the certificates in the batch
-    ipfs_batch_file = "./data/meta_certificates/" + str(uuid.uuid1()) + '.json'
-    #Retrieve file path of certified transaction
-    blockchain_file_path = config.blockchain_certificates_dir
-    json_data = []
-
-    ipfs_object =  {"file_certifications": []}
-    
-    for fileID in certificate_batch_handler.certificates_to_issue:
-        full_path_with_file = str(blockchain_file_path + '/' + fileID + '.json')         
-        ipfsHash = add_file_ipfs(full_path_with_file)
-        #add_file_ipns(ipfsHash)
-
-        with open(full_path_with_file) as f:
-            d = json.load(f)
-        #Save JSON Certificate to IPFS
-        temp = ipfs_object["file_certifications"]
-        #ipfs_list.append('http://ipfs.io/ipfs/' + ipfsHash)
-        y = {"id": fileID, "ipfsHash": 'http://ipfs.io/ipfs/' + ipfsHash, "crid": d["cridHash"]}
-        temp.append(y)
-        json_data.append(d)
-
-    # write ipfs object into the ipfs batch file
-    print(ipfs_object)
-    with open(ipfs_batch_file, 'w') as file:
-        json.dump(ipfs_object, file)
-
-    ipfs_batch_hash = add_file_ipfs(ipfs_batch_file)
-    
-    update_ipfs_link(token_id, 'http://ipfs.io/ipfs/' + ipfs_batch_hash)
 
     return json_data
--- app/cert_tools_api.py
+++ app/cert_tools_api.py
@@ -7,6 +7,9 @@ from pydantic import BaseModel, Field, Json
 from zipfile import ZipFile
 from urllib.error import HTTPError
 import configargparse
+import uuid
+import fitz
+import io
 import os
 import httpx
 import time
@@ -17,30 +20,92 @@ import shutil
 app = FastAPI()
 
 
-
 class Batch(BaseModel):
-    publicKey: str = Field(description='Public bloxberg address where the Research Object Certificate token will be minted')
-    crid: List[str] = Field(description= 'Cryptographic Identifier of each file you wish to certify. One certificate will be generated per hash up to a maximum of 1000 in a single request', max_length=1000)
-    cridType: Optional[str] = Field(description='If crid is not self-describing, provide the type of cryptographic function you used to generate the cryptographic identifier. Plesae use the name field from the multihash list to ensure compatibility: https://github.com/multiformats/multicodec/blob/master/table.csv')
-    enableIPFS: bool = Field(description= 'EXPERIMENTAL: Set to true to enable posting certificate to IPFS. If set to false, will simply return certificates in the response. By default, this is disabled on the server due to performance and storage problems with IPFS')
-    enablePDF: bool = Field(description= 'Set to True if you would like the certificate to be returned as a PDF file')
-    metadataJson: Optional[Json] = Field(description='Provide optional metadata to describe the research object batch in more detail that will be included in the certificate.')
-
+    publicKey: str = Field(
+        description='Public bloxberg address where the Research Object Certificate token will be minted')
+    crid: List[str] = Field(
+        description='Cryptographic Identifier of each file you wish to certify. One certificate will be generated per hash up to a maximum of 1000 in a single request',
+        max_length=1000)
+    cridType: Optional[str] = Field(
+        description='If crid is not self-describing, provide the type of cryptographic function you used to generate the cryptographic identifier.'
+                    ' Please use the name field from the multihash list to ensure compatibility: https://github.com/multiformats/multicodec/blob/master/table.csv')
+    enableIPFS: bool = Field(
+        description='EXPERIMENTAL: Set to true to enable posting certificate to IPFS. If set to false, will simply return certificates in the response.'
+                    ' By default, this is disabled on the server due to performance and storage problems with IPFS')
+    metadataJson: Optional[Json] = Field(
+        description='Provide optional metadata to describe the research object batch in more detail '
+                    'that will be included in the certificate.')
 
     class Config:
         schema_extra = {
             "example": {
                 "publicKey": "0x69575606E8b8F0cAaA5A3BD1fc5D032024Bb85AF",
-                "crid": ["0x0e4ded5319861c8daac00d425c53a16bd180a7d01a340a0e00f7dede40d2c9f6", "0xfda3124d5319861c8daac00d425c53a16bd180a7d01a340a0e00f7dede40d2c9f6"],
+                "crid": ["0x0e4ded5319861c8daac00d425c53a16bd180a7d01a340a0e00f7dede40d2c9f6",
+                         "0xfda3124d5319861c8daac00d425c53a16bd180a7d01a340a0e00f7dede40d2c9f6"],
                 "cridType": "sha2-256",
                 "enableIPFS": False,
-                "enablePDF": True,
                 "metadataJson": "{\"authors\":\"Albert Einstein\"}"
             }
         }
 
+
+class jsonCertificate(BaseModel):
+    context: Optional[List[str]] = Field(
+        description='Relevant JSON-LD context links in order to validate Verifiable Credentials according to their spec.'
+    )
+    id: str
+    type: List[str]
+    issuer: str
+    issuanceDate: str
+    credentialSubject: dict
+    displayHtml: Optional[str]
+    crid: str
+    cridType: Optional[str]
+    metadataJson: Optional[str]
+    proof: dict
+
+
+    class Config:
+        schema_extra = {
+            "example":
+                    {
+                        "@context": [
+                            "https://www.w3.org/2018/credentials/v1",
+                            "https://w3id.org/blockcerts/schema/3.0-alpha/context.json"
+                        ],
+                        "type": [
+                            "VerifiableCredential",
+                            "BlockcertsCredential"
+                        ],
+                        "issuer": "https://raw.githubusercontent.com/bloxberg-org/issuer_json/master/issuer.json",
+                        "issuanceDate": "2020-10-28T14:30:34.011731+00:00",
+                        "credentialSubject": {
+                            "id": "https://blockexplorer.bloxberg.org/address/0x69575606E8b8F0cAaA5A3BD1fc5D032024Bb85AF",
+                            "issuingOrg": {
+                                "id": "https://bloxberg.org"
+                            }
+                        },
+                        "displayHtml": "<section class=\"text\" style=\"margin-top:24px;width:100%;display:inline-block;\"><span style=\"display:block;font-family:Helvetica, sans-serif;font-weight:bold;font-size:2.5em;text-align:left;text-transform:none;color:#4e5f6b;margin:0 auto;width:100%\">Research Object Certificate</span></section><section class=\"text\" style=\"margin-top:12px;width:100%;display:inline-block;\"><span style=\"display:block;font-family:Helvetica, sans-serif;font-weight:normal;font-size:1em;text-align:left;text-transform:none;color:#4e5f6b;float: left;width:100%\">This bloxberg certificate serves as a proof of existence that the data corresponding to the  cryptographic identifier were transacted on the bloxberg blockchain at the issued time.</span></section><h1>bloxberg Certificate</h1><h2></h2></div>",
+                        "id": "https://bloxberg.org",
+                        "crid": "0xfda3124d5319861c8daac00d425c53a16bd180a7d01a340a0e00f7dede40d2c9f6",
+                        "cridType": "sha-256",
+                        "metadataJson": "{\"authors\": \"Albert Einstein\"}",
+                        "proof": {
+                            "type": "MerkleProof2019",
+                            "created": "2020-10-28T14:30:40.469960",
+                            "proofValue": "z2LuLBVSfogU8YhUevw7i7eo94141kAPwfYuY7XGbqmy6PJvWiEvy9Q5C9niJ6B4Cy5PeHo8rC5azvniXW75WxHhziZGRj6jK7G5i3X2EdyurnhSHwTAhjCEo6gE4oFBQUhb65ZcWmidNBVYqbvHCnmFaY7SKiUmmuELmC9dA3Z89X1b1QVquiC8yrqFdMeBptPP8tMk9StHKQfG1X2u4JzWSTmR4RVKnh4XAo8UitRiz8zeQSNZJuQ2kTg2PTMxnigap4US5vVL5UKESKUSB9kAvk1YpBfrzuEtEiVqFWWMk6V48MYkBwP86HnY4yh6LwM31J6c6NyNeUcVmUAjhMFenaFZXoWvkzj6nUmRVcLdRmARkWCAuWikVTbgri4Cw8p7cezHXvE9mmvuC9HYfB",
+                            "proofPurpose": "assertionMethod",
+                            "verificationMethod": "ecdsa-koblitz-pubkey:0xD748BF41264b906093460923169643f45BDbC32e",
+                            "ens_name": "mpdl.berg"
+                        }
+                    }
+                }
+
+class jsonCertificateBatch(BaseModel):
+    __root__: Optional[List[jsonCertificate]]
+
 async def issueRequest(url, headers, payload):
-    response = requests.request("POST", url, headers=headers, data = payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
     encodedResponse = response.text.encode('utf8')
     jsonText = json.loads(encodedResponse)
     # async with httpx.AsyncClient() as client:
@@ -49,62 +114,31 @@ async def issueRequest(url, headers, payload):
     #     jsonText = await json.loads(encodedResponse)
     return jsonText
 
-def zipfiles(filenames):
-    zip_subdir = str(conf.abs_data_dir + '/' + 'pdf_certificates/')
-    zip_filename = "%s.zip" % zip_subdir
-
-    # Open StringIO to grab in-memory ZIP contents
-    s = StringIO.StringIO()
-    # The zip compressor
-    zf = zipfile.ZipFile(s, "w")
-
-    for fpath in filenames:
-        # Calculate path for file in zip
-        fdir, fname = os.path.split(fpath)
-        zip_path = os.path.join(zip_subdir, fname)
-
-        # Add file, at correct path
-        zf.write(fpath, zip_path)
-
-    # Must close zip for all contents to be written
-    zf.close()
-
-    # Grab ZIP file from in-memory, make response with correct MIME-type
-    resp = Response(s.getvalue(), mimetype = "application/x-zip-compressed")
-    # ..and correct content-disposition
-    resp['Content-Disposition'] = 'attachment; filename=%s' % zip_filename
-
-    return resp
-
 # Zip the files from given directory that matches the filter
-def zipFilesInDir(dirName, zipFileName, filter):
-
-   # create a ZipFile object
-   with ZipFile(zipFileName, 'w') as zipObj:
-       # Iterate over all the files in directory
-       for folderName, subfolders, filenames in os.walk(dirName):
-           for filename in filenames:
-               removedExtension = os.path.splitext(filename)[0]
-               if removedExtension in filter:
-                   # create complete filepath of file in directory
-                   filePath = os.path.join(folderName, filename)
-                   # Add file to zip
-                   zipObj.write(filePath, os.path.basename(filePath))
-
-
-
-
-
+def zipfilesindir(dirName, zipFileName, filter=None):
+    # create a ZipFile object
+    with ZipFile(zipFileName, 'w') as zipObj:
+        # Iterate over all the files in directory
+        for folderName, subfolders, filenames in os.walk(dirName):
+            for filename in filenames:
+                removedExtension = os.path.splitext(filename)[0]
+                if removedExtension in filter or filter is None:
+                    # create complete filepath of file in directory
+                    filePath = os.path.join(folderName, filename)
+                    # Add file to zip
+                    zipObj.write(filePath, os.path.basename(filePath))
 
 ##Full Workflow
 @app.post("/createBloxbergCertificate")
 async def createBloxbergCertificate(batch: Batch):
-    #Currently don't support IPFS due to performance and space issues.
+    # Currently don't support IPFS due to performance and space issues.
     if batch.enableIPFS is True:
-        raise HTTPException(status_code=400, detail="IPFS is not supported currently due to performance and storage requirements.")
-    #limit number of CRIDs to 1000
+        raise HTTPException(status_code=400,
+                            detail="IPFS is not supported currently due to performance and storage requirements.")
+    # limit number of CRIDs to 1000
     if len(batch.crid) >= 101:
-        raise HTTPException(status_code=400, detail="You are trying to certify too many files at once, please limit to 1000 files per batch.")
+        raise HTTPException(status_code=400,
+                            detail="You are trying to certify too many files at once, please limit to 1000 files per batch.")
 
     conf = create_v3_alpha_certificate_template.get_config()
 
@@ -121,10 +155,11 @@ async def createBloxbergCertificate(batch: Batch):
     create_v3_alpha_certificate_template.write_certificate_template(conf, batch.publicKey)
     conf_instantiate = instantiate_v3_alpha_certificate_batch.get_config()
     if batch.metadataJson is not None:
-        uidArray = instantiate_v3_alpha_certificate_batch.instantiate_batch(conf_instantiate, batch.publicKey, batch.crid, batch.metadataJson)
+        uidArray = instantiate_v3_alpha_certificate_batch.instantiate_batch(conf_instantiate, batch.publicKey,
+                                                                            batch.crid, batch.metadataJson)
     else:
-        uidArray = instantiate_v3_alpha_certificate_batch.instantiate_batch(conf_instantiate, batch.publicKey, batch.crid)
-
+        uidArray = instantiate_v3_alpha_certificate_batch.instantiate_batch(conf_instantiate, batch.publicKey,
+                                                                            batch.crid)
     end = time.time()
     print(end - start)
     if python_environment == "production":
@@ -132,10 +167,11 @@ async def createBloxbergCertificate(batch: Batch):
     else:
         url = "http://cert_issuer_api:80/issueBloxbergCertificate"
 
-    payload = {"recipientPublickey": batch.publicKey, "unSignedCerts": uidArray, "enableIPFS": batch.enableIPFS, "enablePDF": batch.enablePDF }
+    payload = {"recipientPublickey": batch.publicKey, "unSignedCerts": uidArray, "enableIPFS": batch.enableIPFS
+              }
     headers = {
-  'Content-Type': 'application/json'
-}
+        'Content-Type': 'application/json'
+    }
     payload = json.dumps(payload)
     start2 = time.time()
     print('starting cert-issuance')
@@ -147,36 +183,49 @@ async def createBloxbergCertificate(batch: Batch):
             os.remove(full_path_with_file)
     except Exception as e:
         print(e)
-        for x in uidArray:
-            full_path_with_file = str(conf.abs_data_dir + '/' + 'unsigned_certificates/' + x + '.json')
-            os.remove(full_path_with_file)
-            full_path_with_pdf = str(conf.abs_data_dir + '/' + 'pdf_certificates/' + x + '.pdf')
-            os.remove(full_path_with_pdf)
+        try:
+            for x in uidArray:
+                full_path_with_file = str(conf.abs_data_dir + '/' + 'unsigned_certificates/' + x + '.json')
+                os.remove(full_path_with_file)
+                full_path_with_pdf = str(conf.abs_data_dir + '/' + 'pdf_certificates/' + x + '.pdf')
+                os.remove(full_path_with_pdf)
+        except Exception as e:
+            print(e)
         raise HTTPException(status_code=404, detail="Certifying batch to the blockchain failed.")
     end2 = time.time()
     print(end2 - start2)
     # TODO: Make requests Async
-    if batch.enablePDF is False:
-        return jsonText
-    else:
-        #python_file = shutil.make_archive("../cert_issuer/data/pdf_certificates", 'zip', str(conf.abs_data_dir + '/' + 'pdf_certificates/'))
-        filePathZip = "./sample_data/bloxbergResearchCertificates.zip"
-        zipFilesInDir("./sample_data/pdf_certificates", filePathZip, uidArray)
-        resp = FileResponse(filePathZip, media_type="application/x-zip-compressed")
-        resp.headers['Content-Disposition'] = 'attachment; filename=bloxbergResearchCertificates'
 
-        return resp
+    return jsonText
 
 
 
+@app.post("/generatePDF")
+def generatePDF(request: jsonCertificateBatch):
+    requestJson = request.json()
+    certificateObject = json.loads(requestJson)
+    uidArray = []
+    #for certificate in request.__root__:
+    for certificate in certificateObject:
+        generatedID = str(uuid.uuid1())
+        uidArray.append(generatedID)
+        stringCert = json.dumps(certificate)
+        bytestring = io.StringIO(stringCert)
+        content = io.BytesIO(bytestring.read().encode('utf8'))
+        doc = fitz.open('./bloxbergDataCertificate.pdf')
+        doc.embeddedFileAdd("bloxbergJSONCertificate", content)
+        doc.save('./sample_data/pdf_certificates/' + generatedID + '.pdf', garbage=4, deflate=1)
 
-## Test Certificate Generation Endpoint with issuance
-@app.post("/createUnsignedCertificateBatchTest")
-def createUnsignedCertificate(batch: Batch):
-    #create_v3_alpha_certificate_template(recipient_name, email)
-    conf = create_v3_alpha_certificate_template.get_config()
-    create_v3_alpha_certificate_template.write_certificate_template(conf, batch.recipient_name, batch.email)
-    conf_instantiate = instantiate_v3_alpha_certificate_batch.get_config()
-    uidArray = instantiate_v3_alpha_certificate_batch.instantiate_batch(conf_instantiate, batch.publicKey, batch.recipient_name, batch.email, batch.crid)
+    filePathZip = "./sample_data/bloxbergResearchCertificates.zip"
+    zipfilesindir("./sample_data/pdf_certificates", filePathZip, uidArray)
+    resp = FileResponse(filePathZip, media_type="application/x-zip-compressed")
+    resp.headers['Content-Disposition'] = 'attachment; filename=bloxbergResearchCertificates'
 
-    return uidArray
+    full_path_with_file = str('./sample_data/pdf_certificates/')
+
+    for file_name in os.listdir(full_path_with_file):
+        if file_name.endswith('.pdf'):
+            print(full_path_with_file + file_name)
+            os.remove(full_path_with_file + file_name)
+
+    return resp
